53bd102ad8b1409db53504bd44ee419d3696e0f2
Joni Kanerva <jhonny@jhonny.org>
2021-01-01 09:05:33 +0200

    initial validation indicators

--- src/Time.tsx
+++ src/Time.tsx
@@ -1,11 +1,13 @@
+import './Time.css'
+
 import { DateTime } from 'luxon'
 import React, { Fragment, useEffect, useState } from 'react'
-import { parseTime } from './datetime'
 
+import { parseTime } from './datetime'
 import Display from './Display'
 
 const Time: React.FC = () => {
-  const [timeValue, setTimeValue] = useState<string>('12:00')
+  const [timeValue, setTimeValue] = useState<string>('14')
   const [timeZoneValue, setTimeZoneValue] = useState<string>('UTC')
   const [localTime, setLocalTime] = useState<DateTime>(DateTime.local())
   const [remoteTime, setRemoteTime] = useState<DateTime>(DateTime.local())
@@ -24,14 +26,19 @@ const Time: React.FC = () => {
       <input
         autoFocus
         type="text"
+        className="inputText"
         placeholder="Time"
         value={timeValue}
+        maxLength={5}
+        pattern="([01]?[0-9]|2[0-3]):?([0-5][0-9]){0,2}"
         onChange={(e) => setTimeValue(e.target.value)}
       />
       <input
         type="text"
+        className="inputText"
         placeholder="Time Zone"
         value={timeZoneValue}
+        pattern="[A-Za-z\/]+"
         onChange={(e) => setTimeZoneValue(e.target.value)}
       />
       <Display remoteTime={remoteTime} localTime={localTime} />
